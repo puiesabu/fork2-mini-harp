8e8fb889dbde8981c205c3f1ab596be358161a3c
kaeli <puiesabu@gmail.com>
2014-04-01 21:55:39 +0800

    Implement jade-preprocess

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var makeJade = require('./lib/processor/jade');
 
-module.exports = function(path) {
+module.exports = function(root) {
   var app = connect();
 
-  app.use(serveStatic(path)).use(function(request, response, next) {
+  app.use(function(request, response, next) {
     if (request.url == "/current-time") {
       response.end((new Date()).toISOString());
     } else {
       next();
     }
-  });
+  })
+  	.use(serveStatic(root))
+  	.use(makeJade(root));
 
   return app;
 }
